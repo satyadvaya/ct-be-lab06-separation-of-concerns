55297c06fae32c550381d0a6859d0878ee05e959
satyadvaya <satyadvaya@gmail.com>
2021-09-23 04:32:27 -0700

    failing test for GET all orders

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,28 +3,40 @@ const twilio = require('twilio');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const Order = require('../lib/models/Order.js');
 
 jest.mock('twilio', () => () => ({
-  messages: {
-    create: jest.fn(),
-  },
+    messages: {
+        create: jest.fn(),
+    },
 }));
 
 describe('03_separation-of-concerns-demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+    beforeEach(() => {
+        return setup(pool);
+    });
 
-  it('creates a new order in our database and sends a text message', () => {
-    return request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 })
-      .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
-        expect(res.body).toEqual({
-          id: '1',
-          quantity: 10,
-        });
-      });
-  });
+    it('creates a new order in our database and sends a text message', () => {
+        return request(app)
+            .post('/api/v1/orders')
+            .send({ quantity: 10 })
+            .then((res) => {
+                // expect(createMessage).toHaveBeenCalledTimes(1);
+                expect(res.body).toEqual({
+                    id: '1',
+                    quantity: 10,
+                });
+            });
+    });
+
+    it('gets all orders', async () => {
+        return request(app)
+            .get('/api/v1/orders')
+            .then((res) => {
+                expect(res.body).toEqual({
+                    id: '1',
+                    quantity: 10,
+                });
+            });
+    });
 });
