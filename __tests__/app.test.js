5b7d95c722adc0f7bf512926e2640613b024c4b3
satyadvaya <satyadvaya@gmail.com>
2021-09-23 16:47:38 -0700

    failing test for GET an order by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,4 +41,17 @@ describe('03_separation-of-concerns-demo routes', () => {
                 ]);
             });
     });
+
+    it('should GET an order by id', () => {
+        return request(app)
+            .get('/api/v1/orders/1')
+            .then((res) => {
+                expect(res.body).toEqual([
+                    {
+                        id: '1',
+                        quantity: 10,
+                    },
+                ]);
+            });
+    });
 });
