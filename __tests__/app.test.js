4938f09bc385626bfaab2412bfcb63a2a3c4cdf4
satyadvaya <satyadvaya@gmail.com>
2021-09-23 05:57:09 -0700

    persistently failing test for GET all orders

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,13 +30,20 @@ describe('03_separation-of-concerns-demo routes', () => {
     });
 
     it('gets all orders', async () => {
+        await request(app).post('/api/v1/orders').send({ quantity: 10 });
         return request(app)
             .get('/api/v1/orders')
             .then((res) => {
-                expect(res.body).toEqual({
-                    id: '1',
-                    quantity: 10,
-                });
+                expect(res.body).toEqual([
+                    {
+                        id: '1',
+                        quantity: 10,
+                    },
+                    {
+                        id: '2',
+                        quantity: 10,
+                    },
+                ]);
             });
     });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,4 +3,7 @@ DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   quantity INTEGER CHECK (quantity > 0)
-)
+);
+
+-- INSERT INTO orders (quantity)
+--   VALUES (10);
